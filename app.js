992d1b1941c15a077e4280365f5e401b15a3011a
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-17 15:21:11 -0500

    Added employee photos

--- app.js
+++ app.js
@@ -24,6 +24,37 @@ const getWeb = function () {
     $('#web').addClass('bg-danger');
 };
 
+const getLeadership=function(){
+    $('.tech').addClass('invisible');
+    $('.appraisers').addClass('invisible');
+    $('.support').addClass('invisible');
+    $('.leadership').removeClass('invisible');
+}
+const getTech=function(){
+    $('.leadership').addClass('invisible');
+    $('.appraisers').addClass('invisible');
+    $('.support').addClass('invisible');
+    $('.tech').removeClass('invisible');
+}
+const getAppraisers=function(){
+    $('.leadership').addClass('invisible');
+    $('.tech').addClass('invisible');
+    $('.support').addClass('invisible');
+    $('.appraisers').removeClass('invisible');
+}
+const getSupport=function(){
+    $('.leadership').addClass('invisible');
+    $('.appraisers').addClass('invisible');
+    $('.tech').addClass('invisible');
+    $('.support').removeClass('invisible');
+}
+
 $('#GIS').on('click', getGIS);
 $('#value').on('click', getValuation);
-$('#web').on('click', getWeb)
+$('#web').on('click', getWeb);
+$('.nav-about').hover(function(){
+    $(this).toggleClass('bg-dark')});
+$('#leader').on('click', getLeadership)
+$('#tech').on('click', getTech);
+$('#appraisers').on('click',getAppraisers);
+$('#support').on('click',getSupport);
