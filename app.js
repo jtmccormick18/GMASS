c0935486c4368fe7d4898d482d313867aa01ed85
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-19 10:39:18 -0500

    added more pages

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const getGIS = function () {
     $('.GIS').removeClass('invisible');
     $('.value').addClass('invisible');
-    $('.web').addClass('invisible');
+    $('.projects').addClass('invisible');
     $('#GIS').addClass('bg-danger');
     $('#value').removeClass('bg-danger');
-    $('#web').removeClass('bg-danger');
+    $('#projects').removeClass('bg-danger');
 };
 
 const getValuation = function () {
     $('.value').removeClass('invisible');
     $('.GIS').addClass('invisible');
-    $('.web').addClass('invisible');
+    $('.projects').addClass('invisible');
     $('#GIS').removeClass('bg-danger');
     $('#value').addClass('bg-danger');
-    $('#web').removeClass('bg-danger');
+    $('#projects').removeClass('bg-danger');
 };
-const getWeb = function () {
+const getProjects = function () {
     $('.value').addClass('invisible');
     $('.GIS').addClass('invisible');
-    $('.web').removeClass('invisible');
+    $('.projects').removeClass('invisible');
     $('#GIS').removeClass('bg-danger');
     $('#value').removeClass('bg-danger');
-    $('#web').addClass('bg-danger');
+    $('#projects').addClass('bg-danger');
 };
 
 const getLeadership = function () {
@@ -81,7 +81,7 @@ const submitContact = function (e) {
 
 $('#GIS').on('click', getGIS);
 $('#value').on('click', getValuation);
-$('#web').on('click', getWeb);
+$('#projects').on('click', getProjects);
 $('.nav-about').hover(function () {
     $(this).toggleClass('bg-dark')
 });
